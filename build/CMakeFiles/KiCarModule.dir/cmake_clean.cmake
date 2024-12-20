1d9f86b137bff424f223f0b22e7e2f55fa2a0890
lyt524 <lyt13766794520@163.com>
2024-12-20 14:36:11 +0800

    add #pragma once

---
+++ build/CMakeFiles/KiCarModule.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KiCarModule.dir/bind.cpp.o"
+  "CMakeFiles/KiCarModule.dir/bind.cpp.o.d"
+  "CMakeFiles/KiCarModule.dir/kinematics_model.cpp.o"
+  "CMakeFiles/KiCarModule.dir/kinematics_model.cpp.o.d"
+  "KiCarModule.cpython-310-x86_64-linux-gnu.so"
+  "KiCarModule.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KiCarModule.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/KinematicsModelTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KinematicsModelTest.dir/kinematics_model.cpp.o"
+  "CMakeFiles/KinematicsModelTest.dir/kinematics_model.cpp.o.d"
+  "CMakeFiles/KinematicsModelTest.dir/test.cpp.o"
+  "CMakeFiles/KinematicsModelTest.dir/test.cpp.o.d"
+  "KinematicsModelTest"
+  "KinematicsModelTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KinematicsModelTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
